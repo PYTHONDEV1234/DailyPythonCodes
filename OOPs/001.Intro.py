01ed3cb2634c24f739d0dabd0fe574004f3a4a2c
Arvinder Pal <arvinderpal10@gmail.com>
2024-07-13 18:57:08 +0530

    Intro to Object Oriented Programming in Python

---
+++ OOPs/001.Intro.py
@@ -0,0 +1,16 @@
+#OOPs - Object Oriented Programming 
+# A different style of writing code
+
+
+# Everything is a object. ---> Variable 
+
+myList = [12,34,56,7,89]
+print(type(myList))
+#<class 'list'>
+# Class is blueprint ---> It is not a real life thing --> Car Blueprint
+# We write more codes for Class 
+# Object is an instance of class --> Using car Blueprint -> The manufacturer creates all the cars
+
+myList.append(20)
+#'list' object has no attribute 'add'
+myTouple = (23,46)
