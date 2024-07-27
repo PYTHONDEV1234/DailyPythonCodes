b60bb2fa466e1a646a24e6565e6c5f3a931083fc
Arvinder Pal <arvinderpal10@gmail.com>
2024-07-27 18:57:28 +0530

    Init in OOPs

---
+++ OOPs/006.SelfieApp.py
@@ -0,0 +1,14 @@
+class SelfieApp:
+    def __init__(self):
+        print("Internet connection successful.!!!!!!!")
+        print("Camera enabled successfully.!!!!!!!")
+    
+    def ClickPhoto(self):
+        print("Photo saved to Gallery.")
+    
+    def UploadPhoto(self):
+        print("Upload successfull.!!!!!!!!!!!!!")
+
+user1 = SelfieApp()
+print(user1.ClickPhoto())
+print(user1.UploadPhoto())
